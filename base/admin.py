bfe1460844f5b363aaa73d1ce90d5eca39fae82c
Muhammad Taha <mtaha.dev@gmail.com>
2023-05-25 17:05:44 +0500

    updated department field

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.forms import Select
 
-from base.models import AuditProgramRepo, RiskRepo
+from base.models import AuditProgramRepo, RiskRepo, Department
+
+class DetailsAdmin(admin.ModelAdmin):
+    # fields = ('title', 'department_name', 'description')
+    def cube(num, two):
+      return [two, two]
+    def get_form(self, request, obj=None, **kwargs):
+        # 1. Get the form from the parent class:
+        form = super(DetailsAdmin, self).get_form(request, obj, **kwargs)
+        # 2. Change the widget:
+        org_list = Department.objects.all()
+        form.base_fields['department_name'].widget = Select(choices=list(map(self.cube, org_list)) )
+        # 3. Return the form!
+        return form
 
 # Register your models here.
-admin.site.register(RiskRepo)
+admin.site.register(RiskRepo, DetailsAdmin)
 admin.site.register(AuditProgramRepo)
+admin.site.register(Department)
--- base/models.py
+++ base/models.py
@@ -12,8 +12,20 @@ class AuditProgramRepo(models.Model):
     summary = models.TextField(null=True, blank=True)
     description = models.TextField(blank=True)
     category = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+       return self.title
 
 class RiskRepo(models.Model):
    title = models.CharField(max_length=30, null=True, blank=True)
    department_name = models.CharField(max_length=30, null=True, blank=True)
    description = models.TextField()  
+
+   def __str__(self):
+       return self.title
+
+class Department(models.Model):
+   department_name = models.CharField(max_length=30, null=True, blank=True)
+
+   def __str__(self):
+       return self.department_name
+  
